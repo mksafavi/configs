bca3fdd893ef76ca6faf083ad326b642e025c725
mksafavi <mksafavi@gmail.com>
2024-02-22 22:50:15 +0330

    enabled ssh

--- system/machine/t800/default.nix
+++ system/machine/t800/default.nix
@@ -137,13 +137,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
