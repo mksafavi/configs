8ceb85e0f663a8326edca0c371f832046a878479
mksafavi <mksafavi@gmail.com>
2024-03-07 01:41:47 +0330

    t800: enabled wayland on sddm hoping it would fix the no signal on logout issue

--- system/t800/default.nix
+++ system/t800/default.nix
@@ -36,6 +36,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
 
   # Enable OpenGL
