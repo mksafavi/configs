2279d5f87f45497f2e61f25be3dd9d1ca2d257e5
mksafavi <mksafavi@gmail.com>
2024-03-10 17:53:54 +0330

    t1000: enabled lactd systemd service

--- system/t1000/configuration.nix
+++ system/t1000/configuration.nix
@@ -124,6 +124,7 @@
     wget
   ];
   systemd.packages = with pkgs; [ lact ];
+  systemd.services.lact.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
