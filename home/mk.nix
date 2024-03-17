d32c4716597da5ff156f026381e502c1515abdd8
mksafavi <mksafavi@gmail.com>
2024-03-17 21:20:09 +0330

    enabled fish as interactive shell

--- home/mk.nix
+++ home/mk.nix
@@ -21,5 +21,15 @@
     enableBashIntegration = true; # see note on other shells below
     nix-direnv.enable = true;
   };
-  programs.bash.enable = true;
+  programs.fish.enable = true;
+  programs.bash = {
+  enable = true;
+  initExtra= ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+};
 }
