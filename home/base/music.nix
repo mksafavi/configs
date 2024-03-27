64b12c0c0663eaca804ed957c042dbbc841680b2
mksafavi <mksafavi@gmail.com>
2024-03-28 00:21:36 +0330

    installed reaper's reapack from url

--- home/base/music.nix
+++ home/base/music.nix
@@ -4,4 +4,10 @@
     yabridgectl
     reaper
   ];
+
+  home.file.".config/REAPER/UserPlugins/reaper_reapack-x86_64.so".source = pkgs.fetchurl {
+    url = "https://github.com/cfillion/reapack/releases/download/v1.2.4.5/reaper_reapack-x86_64.so";
+    hash = "sha256-h3r3tF3XRjqY3Xy57Uaypg3pdJbyZ/zTyzF2DFmEJEA=";
+  };
+
 }
