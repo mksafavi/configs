e8fc9d656d7dab89cb052091248465cbf9a80095
mksafavi <mksafavi@gmail.com>
2024-03-31 05:17:36 +0330

    removed yuzu

--- home/base/gaming.nix
+++ home/base/gaming.nix
@@ -2,7 +2,7 @@
   home.packages = with pkgs; [ 
     (lutris.override { extraPkgs = pkgs: [ wineWowPackages.stable ]; })
     mangohud
-    yuzu
+    #yuzu # TODO: install this from the old nixpkgs rev
     ryujinx
   ];
 }
