1fc7e35e88586864af4250e3af09c714a88f11b7
mksafavi <mksafavi@gmail.com>
2024-04-22 00:42:05 +0330

    set nixPath

--- system/configuration.nix
+++ system/configuration.nix
@@ -25,8 +25,8 @@
 
     # Flakes settings
     package = pkgs.nixUnstable;
-    registry.nixpkgs.flake = nixpkgs;
-    nixPath = [ "nixpkgs=flake:nixpkgs" ];
+    registry.nixpkgs.flake = nixpkgs; # use nixpkgs for flake commands instead of downloading flake-registry
+    nixPath = [ "nixpkgs=${nixpkgs}" ]; # use nixpkgs for legacy commands instead of downloading flake-registry
     settings = {
       experimental-features = [
         "nix-command"
