e65c7a3a88639c148a5067f5cef44b6878b67646
mksafavi <mksafavi@gmail.com>
2024-06-30 01:19:11 +0330

    enabled git lfs

--- system/t800/configuration.nix
+++ system/t800/configuration.nix
@@ -146,9 +146,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git
     wget
   ];
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
