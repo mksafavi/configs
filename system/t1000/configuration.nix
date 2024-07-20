816e51acb969022fe934186b3832866bed2bc4fa
mksafavi <mksafavi@gmail.com>
2024-07-20 21:47:59 +0330

    set sddm default to wayland

--- system/t1000/configuration.nix
+++ system/t1000/configuration.nix
@@ -51,6 +51,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
