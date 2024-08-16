b2aa4bc6d8ab694c256f60e4d921edb44b279e66
mksafavi <mksafavi@gmail.com>
2024-08-16 04:27:10 +0330

    t1000: set default session to plasmawayland

--- system/t1000/configuration.nix
+++ system/t1000/configuration.nix
@@ -56,6 +56,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.defaultSession = "plasma";
   services.desktopManager.plasma6.enable = true;
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
