37f9de844de35f9f57136061d9a5a3b421960583
mksafavi <mksafavi@gmail.com>
2024-08-21 05:42:59 +0330

    added mpv uosc and mpris plugins

--- home/base/media.nix
+++ home/base/media.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    mpv
-    vlc
-  ];
+  home.packages = with pkgs; [ vlc ];
+  programs.mpv = {
+    enable = true;
+    scripts = with pkgs; [
+      mpvScripts.uosc
+      mpvScripts.mpris
+    ];
+  };
 }
