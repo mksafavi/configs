745fd70778ba7e0e5da4b1899cab9daf275ea2f5
mksafavi <mksafavi@gmail.com>
2024-11-23 20:49:51 +0330

    t800: switched to beta nvidia drivers

--- system/t800/configuration.nix
+++ system/t800/configuration.nix
@@ -97,7 +97,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # Configure keymap in X11
