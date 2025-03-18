a310b592e1caea27afeb459e6ea4821f0957038c
mksafavi <mksafavi@gmail.com>
2025-03-19 00:52:00 +0330

    t800: separated hardware.nix for gpu and other devices configs

--- system/t800/configuration.nix
+++ system/t800/configuration.nix
@@ -7,6 +7,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./hardware.nix
     ../base/nix-configuration.nix
     ../base/desktop.nix
     ./audio.nix
@@ -29,59 +30,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable logitech k400 support
-  hardware.logitech.wireless.enable = true;
-  hardware.logitech.wireless.enableGraphical = true;
-
-  hardware.new-lg4ff.enable = true;
-
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  boot.initrd.kernelModules = [
-    "nvidia"
-    "nvidia_uvm"
-    "nvidia_drm"
-    "nvidia_modeset"
-  ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11_beta ];
-
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-    # Fix screen tearing
-    forceFullCompositionPipeline = true;
-    # to enable gpu in headless mode
-    nvidiaPersistenced = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = true;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
-
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
