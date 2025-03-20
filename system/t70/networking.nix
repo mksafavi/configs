e2426161b1fbdb0466b19e43d85100d7432509bf
mksafavi <mksafavi@gmail.com>
2025-03-21 02:11:20 +0330

    t70: enabled aria2 services

--- system/t70/networking.nix
+++ system/t70/networking.nix
@@ -19,6 +19,25 @@
     };
   };
 
+  services.aria2 = {
+    enable = true;
+    openPorts = true;
+    rpcSecretFile = "/var/lib/aria2/aria2-rpc-token";
+    settings =
+      let
+        session-path = "/var/lib/aria2/aria2.session";
+      in
+      {
+        continue = true;
+        max-connection-per-server = 5;
+        save-session = session-path;
+        input-file = session-path;
+        save-session-interval = 10;
+        listen-port = [ ];
+        rpc-listen-port = 6800;
+        rpc-listen-all = true;
+      };
+  };
   services.zerotierone.enable = true;
 
   networking.firewall.allowedTCPPorts = [
