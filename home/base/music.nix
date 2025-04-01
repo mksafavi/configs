f1db21632db372f3d518ecbb55f8a3faf9eaee68
mksafavi <mksafavi@gmail.com>
2025-04-01 23:22:14 +0330

    added reaper-sws-extension and reaper-reapack-extension.
    replaced reaper_reapack binary with the upstream packages

--- home/base/music.nix
+++ home/base/music.nix
@@ -8,8 +8,14 @@
     noise-repellent
   ];
 
-  home.file.".config/REAPER/UserPlugins/reaper_reapack-x86_64.so".source = pkgs.fetchurl {
-    url = "https://github.com/cfillion/reapack/releases/download/v1.2.4.5/reaper_reapack-x86_64.so"; # TODO: this binary might stop working at some point.
-    hash = "sha256-h3r3tF3XRjqY3Xy57Uaypg3pdJbyZ/zTyzF2DFmEJEA=";
+  xdg.configFile."REAPER" = {
+    source = pkgs.symlinkJoin {
+      name = "reaper-userplugins";
+      paths = with pkgs; [
+        reaper-sws-extension
+        reaper-reapack-extension
+      ];
+    };
+    recursive = true;
   };
 }
