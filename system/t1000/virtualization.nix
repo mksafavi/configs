8cd3a9e67758fde836424c067df68e51ef18e79d
mksafavi <mksafavi@gmail.com>
2025-06-05 13:48:49 +0330

    t1000: added podman

--- system/t1000/virtualization.nix
+++ system/t1000/virtualization.nix
@@ -1,4 +1,14 @@
 { config, pkgs, ... }:
 {
-  virtualisation.docker.enable = true;
+  virtualisation = {
+
+    docker.enable = true;
+    podman.enable = true;
+  };
+
+  users.users.mk = {
+    packages = with pkgs; [
+      podman-compose
+    ];
+  };
 }
