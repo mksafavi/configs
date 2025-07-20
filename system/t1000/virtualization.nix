ce59811881344967490913caddbfc98606785cdf
mksafavi <mksafavi@gmail.com>
2025-07-20 06:06:18 +0330

    t1000: added traggo oci container

--- system/t1000/virtualization.nix
+++ system/t1000/virtualization.nix
@@ -7,6 +7,16 @@
     libvirtd.enable = true;
   };
 
+  virtualisation.oci-containers = {
+    containers = {
+      traggo = {
+        image = "traggo/server:0.7.1";
+        ports = [ "127.0.0.1:3030:3030" ];
+        volumes = [ "${config.users.users.mk.home}/.local/share/traggo:/opt/traggo/data" ];
+      };
+    };
+  };
+
   users.users.mk = {
     packages = with pkgs; [
       podman-compose
