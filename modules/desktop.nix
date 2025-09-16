ae72e502bafc7c8531a6b4518f98a94d12e4e72b
mksafavi <mksafavi@gmail.com>
2025-09-16 09:46:18 +0330

    add hyprland to desktop module

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -15,25 +15,45 @@
         '';
       };
     };
+    modules.desktop.hypr = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = lib.mdDoc ''
+          Whether to enable Hyprland desktop
+        '';
+      };
+    };
   };
 
   config =
     let
       cfg = config.modules.desktop;
     in
-    lib.mkIf cfg.kde.enable {
-      services.displayManager.sddm.enable = true;
-      services.displayManager.sddm.wayland.enable = true;
-      services.displayManager.defaultSession = "plasma";
-      services.desktopManager.plasma6.enable = true;
-      services.speechd.enable = false;
-      services.orca.enable = false;
+    lib.mkMerge [
+      (lib.mkIf cfg.kde.enable {
+        services.displayManager.sddm.enable = true;
+        services.displayManager.sddm.wayland.enable = true;
+        services.displayManager.defaultSession = "plasma";
+        services.desktopManager.plasma6.enable = true;
+        services.speechd.enable = false;
+        services.orca.enable = false;
 
-      # Configure keymap in X11
-      services.xserver = {
-        xkb.layout = "us";
-        xkb.variant = "";
-      };
+        # Configure keymap in X11
+        services.xserver = {
+          xkb.layout = "us";
+          xkb.variant = "";
+        };
 
-    };
+      })
+      (lib.mkIf cfg.hypr.enable {
+        services.hypridle.enable = true;
+        programs.hyprlock.enable = true;
+        programs.hyprland = {
+          enable = true;
+          withUWSM = true;
+
+        };
+      })
+    ];
 }
