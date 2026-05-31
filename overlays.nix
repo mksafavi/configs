faa2cf52e155f66e90caf5078d54b48c7aac9f12
mksafavi <mksafavi@gmail.com>
2026-05-31 05:26:35 +0330

    Skip tests in openldap to fix build

--- overlays.nix
+++ overlays.nix
@@ -7,5 +7,14 @@
       nix-profile-diff = (prev.callPackage ./scripts/nix-profile-diff.nix { });
     })
     inputs.fjordlauncher.overlays.default
+    (final: prev: { # Revert when https://github.com/NixOS/nixpkgs/pull/525133 is merged
+      openldap = prev.openldap.overrideAttrs (old: {
+        preCheck =
+          old.preCheck
+          + prev.lib.optionalString prev.stdenv.hostPlatform.isi686 ''
+            rm -f tests/scripts/test*-sync*
+          '';
+      });
+    })
   ];
 }
